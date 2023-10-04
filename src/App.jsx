3433e2960a87c651447b7665f20c1aff440744b9
soar1219 <tsubasa1219.info@gmail.com>
2023-10-05 00:19:42 +0900

    Routerの設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import './App.css';
+import { BrowserRouter, Link, Switch } from 'react-router-dom';
+
 import { Home } from './Home';
 import { Page1 } from './Page1';
 import { Page2 } from './Page2';
+import './App.css';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Page1 />
-      <Page2 />
-    </div>
+    <BrowserRouter> {/* Routerで囲った範囲の中でRouterを使えるようにする */}
+      <div className="App">
+        <Link to="/">HOME</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch> {/* どのパスでどのコンポーネントを出すか記述していく */}
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
+    
   );
 }
 
