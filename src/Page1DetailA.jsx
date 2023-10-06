7259d719929aa60dd70fcff3c0a3a91ae92e2aff
soar1219 <tsubasa1219.info@gmail.com>
2023-10-06 12:52:00 +0900

    Linkを使わない画面遷移

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,18 @@
-import { useLocation } from "react-router-dom"
+import { useLocation, useHistory } from "react-router-dom"
 
 export const Page1DetailA = () => {
     const {state} = useLocation();
     console.log(state)
+
+    const history = useHistory();
+
+    const onClickBack = () => {
+        history.goBack(); /* goBackは戻る機能 */
+    }
     return(
         <>
             <h1>Page1DetailAです</h1>
+            <button onClick={onClickBack}>戻る</button>
         </>
     )
 }
